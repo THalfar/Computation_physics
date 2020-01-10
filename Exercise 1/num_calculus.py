fcb978736528a5983087ff2cf9dcfb432b30f64e
Tobias Halfar <tobias.halfar@tuni.fi>
2020-01-10 21:04:25 +0200

    Update num_calculus.py
    Parannettu tulostusta ja lisätty rajojen ja välien säätö

--- Exercise 1/num_calculus.py
+++ Exercise 1/num_calculus.py
@@ -129,19 +129,19 @@ def test_first_derivate(x, dx):
    oikea =  2*x+3
    testi = first_derivate(testifun1, x, dx)
    erotus = np.abs(oikea-testi)
-   print("Erotus f1'  oikeaan tulokseen oli {}".format(erotus))
+   print("f1'({}) dx: {} virhe abs.: {}".format(x,dx,erotus))
    
    # f2(x) = 2x^3+xˆ2+42 -> 6xˆ2 + 2x   
    oikea =  6*x**2 + 2*x
    testi = first_derivate(testifun2, x, dx)
    erotus = np.abs(oikea-testi)
-   print("Erotus f2'  oikeaan tulokseen oli {}".format(erotus))
+   print("f2'({}) dx: {} virhe abs.: {}".format(x, dx, erotus))
    
     # f3(x) = exp(2x) + sin(x) -> 2*exp(2x) - cos(x)
    oikea = 2* np.exp(2*x) + np.cos(x)
    testi = first_derivate(testifun3, x, dx)
    erotus = np.abs(oikea-testi)
-   print("Erotus f3' oikeaan tulokseen oli {}".format(erotus))
+   print("f3'({}) dx: {} virhe abs.: {}".format(x, dx, erotus))
 
 """
 Testing second derivate with three function
@@ -158,78 +158,79 @@ def test_second_derivate(x, dx):
     oikea = 2
     testi = second_derivate(testifun1, x, dx)
     erotus = np.abs(oikea-testi)
-    print("Erotus f1'' oikeaan tulokseen oli {}".format(erotus))
+    print("f1''({}) dx: {} virha abs: {}".format(x,dx,erotus))
     
     # f2(x) = 2x^3+xˆ2+42 -> 12x + 2  
     oikea = 12*x+2
     testi = second_derivate(testifun2, x, dx)
     erotus = np.abs(oikea-testi)
-    print("Erotus f2'' oikeaan tulokseen oli {}".format(erotus))
+    print("f2''({}) dx: {} virhe abs: {}".format(x,dx,erotus))
     
     # f3(x) = exp(2x) + sin(x) -> 2*exp(2x) - sin(x)
     oikea = 4*np.exp(2*x) - np.sin(x)
     testi = second_derivate(testifun3, x, dx)
     erotus = np.abs(oikea-testi)
-    print("Erotus f3'' oikeaan tulokseen oli {}".format(erotus))
+    print("f3''({}) dx: {} virhe abs: {}".format(x,dx,erotus))
    
 
-def test_integral():
+def test_integral(jako, xmin, xmax):
         
-    x = np.linspace(0,1, 123)
+    jako = 11
+    x = np.linspace(xmin,xmax, jako)
     
     # f1(x) = x^2 + 3x -> 1/3 * x^3 + 3/2 * x^2
     oikea = ( 1/3 * x[-1]**3 + 3/2 * x[-1]**2 ) -  ( 1/3 * x[0]**3 + 3/2 * x[0]**2 ) 
     testi = riemann_sum(x, testifun1)
     erotus = np.abs(oikea-testi)
-    print("Erotus F1 oikeaan tulokseen riemannin summalla oli {}".format(erotus))
+    print("Rieman: F1({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1], jako , erotus))
         
     # f2(x) = 2x^3+xˆ2+42 -> 1/2 * x^4 + 1/3 x^3 + 42x 
     oikea = (1/2 * x[-1]**4 + 1/3 * x[-1]**3 + 42*x[-1] ) - ( 1/2 * x[0]**4 + 1/3 * x[0]**3 + 42*x[0] )
     testi = riemann_sum(x, testifun2)
     erotus = np.abs(oikea-testi)
-    print("Erotus F2 oikeaan tulokseen riemannin summalla oli {}".format(erotus))
+    print("Rieman: F2({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1], jako , erotus))
     
     # f3(x) = exp(2x) + sin(x) -> 1/2 exp(2x) - cos(x)
     oikea = ( 1/2 * np.exp(2*x[-1]) - np.cos(x[-1]) ) - ( 1/2 * np.exp(2*x[0]) - np.cos(x[0]) )
     testi = riemann_sum(x, testifun3)
     erotus = np.abs(oikea-testi)
-    print("Erotus F3 oikeaan tulokseen riemannin summalla oli {}".format(erotus))
+    print("Rieman: F3({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1], jako , erotus))
     
     # f1(x) = x^2 + 3x -> 1/3 * x^3 + 3/2 * x^2
     oikea = ( 1/3 * x[-1]**3 + 3/2 * x[-1]**2 ) -  ( 1/3 * x[0]**3 + 3/2 * x[0]**2 ) 
     testi = trapezoid_int(x, testifun1)
     erotus = np.abs(oikea-testi)
-    print("Erotus F1 oikeaan tulokseen trapedoizilla oli {}".format(erotus))
+    print("Trapezoid: F1({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1], jako , erotus))
         
     # f2(x) = 2x^3+xˆ2+42 -> 1/2 * x^4 + 1/3 x^3 + 42x 
     oikea = (1/2 * x[-1]**4 + 1/3 * x[-1]**3 + 42*x[-1] ) - ( 1/2 * x[0]**4 + 1/3 * x[0]**3 + 42*x[0] )
     testi = trapezoid_int(x, testifun2)
     erotus = np.abs(oikea-testi)
-    print("Erotus F2 oikeaan tulokseen trapedoizilla oli {}".format(erotus))
+    print("Trapezoid: F2({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1], jako , erotus))
     
     # f3(x) = exp(2x) + sin(x) -> 1/2 exp(2x) - cos(x)
     oikea = ( 1/2 * np.exp(2) - np.cos(1) ) - ( 1/2 * np.exp(0) - np.cos(0) )
     testi = trapezoid_int(x, testifun3)
     erotus = np.abs(oikea-testi)
-    print("Erotus F3 oikeaan tulokseen trapedoizilla oli {}".format(erotus))
+    print("Trapezoid: F3({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1], jako , erotus))
         
     # f1(x) = x^2 + 3x -> 1/3 * x^3 + 3/2 * x^2
     oikea = ( 1/3 * x[-1]**3 + 3/2 * x[-1]**2 ) -  ( 1/3 * x[0]**3 + 3/2 * x[0]**2 ) 
     testi = simpson_int(x, testifun1)
     erotus = np.abs(oikea-testi)
-    print("Erotus F1 oikeaan tulokseen simpsonilla oli {}".format(erotus))
+    print("Simpson: F1({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1],jako, erotus))
     
     # f2(x) = 2x^3+xˆ2+42 -> 1/2 * x^4 + 1/3 x^3 + 42x 
     oikea = (1/2 * x[-1]**4 + 1/3 * x[-1]**3 + 42*x[-1] ) - ( 1/2 * x[0]**4 + 1/3 * x[0]**3 + 42*x[0] )
     testi = simpson_int(x, testifun2)
     erotus = np.abs(oikea-testi)
-    print("Erotus F2 oikeaan tulokseen simpsonilla oli {}".format(erotus))
+    print("Simpson: F2({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1],jako, erotus))
        
     # f3(x) = exp(2x) + sin(x) -> 1/2 exp(2x) - cos(x)
     oikea = ( 1/2 * np.exp(2*x[-1]) - np.cos(x[-1]) ) - ( 1/2 * np.exp(2*x[0]) - np.cos(x[0]) )
     testi = simpson_int(x, testifun3)
     erotus = np.abs(oikea-testi)
-    print("Erotus F3 oikeaan tulokseen simpsonilla oli {}".format(erotus))
+    print("Simpson: F3({} , {}) jako: {} virhe abs.: {}".format(x[0], x[-1],jako, erotus))
         
         
 def main():
@@ -237,10 +238,10 @@ def main():
     test_first_derivate(1.42, 0.0001)
     test_second_derivate(1.42, 0.0001)
     
-#    test_first_derivate(-1.42, 0.001)
-#    test_second_derivate(-1.42, 0.001)
+    test_first_derivate(-1.42, 0.001)
+    test_second_derivate(-1.42, 0.001)
     
-    test_integral()
+    test_integral(41, 0, 1)
     
 if __name__ == "__main__":
     main()
