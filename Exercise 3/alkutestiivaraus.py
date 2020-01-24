33b630e084048df6bb8b66a27bdd6f99ad7c974f
Tobias Halfar <tobias.halfar@tuni.fi>
2020-01-24 04:54:27 +0200

    ympyrä ja viiva
    ympyrä ja viiva lisätty järkevästi

--- Exercise 3/alkutestiivaraus.py
+++ Exercise 3/alkutestiivaraus.py
@@ -22,28 +22,60 @@ import matplotlib.pyplot as plt
 
 # q = np.array([[0.5,5.5,1e-5], [1.1,2.1,0]])
 
-def line(l, n, q):
+def viiva(alku, loppu, n, q):
+    
+    if n<3:
+        print("ei alle n<3")
+        return
     
     qn = q/n
-    x = np.linspace(-l/2, l/2, n).reshape((n,1))
-    y = np.zeros((n,1))
-    q = np.ones((n,1))*qn 
-    ulos = np.concatenate((x,y,q), axis = 1)
-    return ulos
+    
+    viiva = np.zeros((n,3))
+    viiva[0, :] = np.append(alku, qn) # add head
+    # viiva[-1,:] = np.append(loppu,qn) # add tail
+    erotus = (loppu - alku) / (n-1)
+    
+    for i in range(1,n):
+        viiva[i, :] = np.append(alku+erotus*i, qn)
+    
+    return viiva
+        
+        
+    
+    
+    
+    
     
 
-def ympyra(n,q):
+
+
+# def line(l, n, q):
     
-    qn = q/n
-    x = np.linspace(np.pi, 2*np.pi,n).reshape((n,1))
-    y = np.sin(x)
-    q = np.ones((n,1))*qn
-    ulos = np.concatenate((x,y,q), axis = 1)
-    return ulos
+#     qn = q/n
+#     x = np.linspace(-l/2, l/2, n).reshape((n,1))
+#     y = np.zeros((n,1))
+#     q = np.ones((n,1))*qn 
+#     ulos = np.concatenate((x,y,q), axis = 1)
+#     return ulos
+    
+
+def ympyra(paikka, alkukulma, loppukulma, r, n, q):
+    
+    qn = q/n    
+    kulma = np.linspace(alkukulma,loppukulma,n)
+    
+    viiva = np.zeros((n,3))
+    for i,aste in enumerate(kulma):
+        lisays = paikka + r * np.array([np.cos(aste), np.sin(aste)])
+        viiva[i,:] = np.append(lisays, qn)
+    
+    return viiva
+
+
     
 
 
-def test(x,y, varaukset, tol):
+def test(x,y, varaukset):
     
     Ex = np.zeros_like(x)
     Ey = np.zeros_like(y)
@@ -53,31 +85,30 @@ def test(x,y, varaukset, tol):
         etaisyys = np.sqrt((varaus[0]-x)**2 + (varaus[1]-y)**2 )
         
         erotusvektoriX =  x- varaus[0] 
-        Ex += varaus[-1] * np.divide( erotusvektoriX, etaisyys**3, out = np.zeros_like(etaisyys),where=etaisyys>tol)
+        Ex += varaus[-1] * np.divide( erotusvektoriX, etaisyys**3, out = np.zeros_like(etaisyys),where=etaisyys>0)
         
         erotusvektoriY = y - varaus[1] 
-        Ey += varaus[-1] * np.divide( erotusvektoriY, etaisyys**3, out = np.zeros_like(etaisyys), where=etaisyys>tol)
+        Ey += varaus[-1] * np.divide( erotusvektoriY, etaisyys**3, out = np.zeros_like(etaisyys), where=etaisyys>0)
         
     return Ex, Ey
         
 
-# varaukset = line(1,1000,1e-6)
 
-x = np.linspace(2,8,20)
-y = np.linspace(-2,2,20)
+x = np.linspace(-3,3,20)
+y = np.linspace(-3,3,20)
 X,Y = np.meshgrid(x,y)
 
-# varaukset = ympyra(100,-1)
-
-# varaukset = np.array([[1,1,1],[]])
-
 fig, ax = plt.subplots()
-# ax.plot(varaukset[:,0], varaukset[:,1], 'bo')
 
-uusia = np.array([[4,1,0.2], [6,1,0.2]])
-varaukset = uusia
-# varaukset = np.concatenate((varaukset,uusia), axis = 0)
-ax.plot([4,6], [1, 1], 'ro')
-Ex, Ey = test(X,Y, varaukset, 0.3)
+alku = np.array([0,0])
+varaukset = ympyra(alku,np.pi,2*np.pi, 1.33, 100,-1)
+ax.plot(varaukset[:,0], varaukset[:,1], 'bo')
+
+silmat = np.array([[-0.5,1,0.5],[0.5,1,0.5]])
+varaukset = np.concatenate((varaukset, silmat), axis = 0)
+
+ax.plot(varaukset[-2:,0], varaukset[-2:,1], 'ro')
+
+Ex, Ey = test(X,Y, varaukset)
 q = ax.quiver(x, y, Ex, Ey)
 
